e4619ebb971a3f6ab7985124797998d52fa41469
Roger Steve Ruiz <hi@rog.gr>
2021-05-25 16:15:48 -0400

    Add in shrugText function

--- install.sh
+++ install.sh
@@ -31,9 +31,15 @@ checkForFile () {
   fi
 }
 
+shrugText () {
+  body=$1
+  shrug="¯\_(ツ)_/¯"
+  echo "${shrug} ${body} ${shrug}"
+}
+
 clear
 echo
-echo "¯\_(ツ)_/¯ Installing the binaries straight up ¯\_(ツ)_/¯"
+shrugText "Installing the binaries straight up"
 echo
 # shellcheck disable=SC2045
 for file in $(ls ./bin/*)
@@ -49,32 +55,32 @@ done
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Checking Lint files ¯\_(ツ)_/¯"
+shrugText "Checking Lint files"
 echo
 checkForFile ./lint/eslintrc "$HOME/.eslintrc"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Checking for git files ¯\_(ツ)_/¯"
+shrugText "Checking for git files"
 echo
 checkForFile ./git/gitconfig "$HOME/.gitconfig"
 checkForFile ./git/gitignore_global "$HOME/.gitignore_global"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Copying git_template files straight up ¯\_(ツ)_/¯"
+shrugText "Copy git_template files straight up"
 echo
 cp -vr -n ./git/git_template "$HOME/.git_template"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Copying custom terminal theme ¯\_(ツ)_/¯"
+shrugText "Checking custom terminal theme"
 echo
 checkForFile ./oh-my-zsh/custom/real.zsh-theme "$HOME/.oh-my-zsh/custom/themes/real.zsh-theme"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Checking for Shell profiles ¯\_(ツ)_/¯"
+shrugText "Checking for Shell profiles"
 echo
 checkForFile ./rc/zshrc "$HOME/.zshrc"
 checkForFile ./rc/aliases "$HOME/Developer/.aliases"
@@ -82,13 +88,13 @@ checkForFile ./rc/variables "$HOME/Developer/.variables"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Checking for ssh configuration ¯\_(ツ)_/¯"
+shrugText "Checking for SSH configuration files"
 echo
 checkForFile ./ssh/config "$HOME/.ssh/config"
 echo
 
 sleep 1
-echo "¯\_(ツ)_/¯ Checking for tmux configuration ¯\_(ツ)_/¯"
+shrugText "Checking for Tmux configuration files"
 echo
 checkForFile ./tmux/tmux.conf "$HOME/.tmux.conf"
 checkForFile ./tmux/tmuxline.snapshot "$HOME/.tmuxline.snapshot"
@@ -103,6 +109,6 @@ checkForFile ./vim/vimrc "$HOME/.vimrc"
 checkForFile ./vim/vimrc.bundles "$HOME/.vimrc.bundles"
 echo
 
-echo "¯\_(ツ)_/¯ Thanks for playing ¯\_(ツ)_/¯"
+shrugText "Thanks for playing"
 sleep 1
 exit 0
