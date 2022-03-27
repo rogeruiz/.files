507ee831e085065ec640a2a0c88021c1b07fa8b3
Roger Steve Ruiz <hi@rog.gr>
2022-03-26 20:40:17 -0400

    Update Fugitive config

--- nvim/lua/configs/fugitive.lua
+++ nvim/lua/configs/fugitive.lua
@@ -2,6 +2,7 @@ vim.cmd([[
   nnoremap <silent> <leader>gs :Git<CR>
   nnoremap <silent> <leader>gc :Git commit -v<CR>
   nnoremap <silent> <leader>gp :Git push<CR>
+  nnoremap <silent> <leader>gbb :Git blame<CR>
   " Mnemonic _i_nteractive
   nnoremap <silent> <leader>gi :Git add -p %<CR>
 ]])
