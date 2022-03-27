927780b40124cdd90a6d7cbd36f29c41281ca80d
Roger Steve Ruiz <hi@rog.gr>
2022-03-26 20:42:04 -0400

    Add Vista config

---
+++ nvim/lua/configs/vista.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+" Shortcut for Vista
+nmap <F8> :Vista!!<CR>
+let g:vista_default_executive = 'nvim_lsp'
+
+" Ensure you have installed some decent font to show these pretty symbols, then you can enable icon for the kind.
+let g:vista#renderer#enable_icon = 1
+
+" The default icons can't be suitable for all the filetypes, you can extend it as you wish.
+let g:vista#renderer#icons = {
+\   "function": "\uf794",
+\   "variable": "\uf71b",
+\  }
+]])
