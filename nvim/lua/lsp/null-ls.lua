8949aae57a08c901d1f3625c334326361df796e7
Roger Steve Ruiz <hi@rog.gr>
2022-04-09 20:33:04 -0400

    Lots of LSP changes but a few to focus on;
    - I know understand how to disable when I have LSP servers setup to
      format things. Thanks to @tjdevries
    - Most of these changes are running the Lua format on these files
    - Null-ls handles Terraform formatting
    - Null-ls now handles Nix diagnostics

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,11 +1,9 @@
-local null_ls_status_ok, null_ls = pcall(require, 'null-ls')
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 local hover = null_ls.builtins.hover
@@ -13,32 +11,32 @@ local hover = null_ls.builtins.hover
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 -- npm install --save-dev prettier prettier-plugin-solidity
 null_ls.setup({
-  debug = false,
-  update_on_insert = true,
-  sources = {
-    formatting.black.with { extra_args = { '--fast' } },
-    formatting.stylua,
-    formatting.terrafmt,
-    formatting.prettier,
-    diagnostics.eslint,
-    diagnostics.checkmake,
-    code_actions.eslint,
-    code_actions.gitsigns,
-    hover.dictionary,
-    diagnostics.vale.with({
-      args = function(params)
-        return {
-          --'--config=' .. home .. '/.config/vale/vale.ini',
-          '--no-exit',
-          '--output',
-          'JSON',
-          '--ext',
-          '.' .. vim.fn.fnamemodify(params.bufname, ':e'),
-        }
-      end,
-    }),
-  },
-  on_attach = function(_) -- client
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-  end
+	debug = false,
+	update_on_insert = true,
+	sources = {
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		formatting.terraform_fmt,
+		formatting.prettier,
+		diagnostics.eslint,
+		diagnostics.statix,
+		code_actions.eslint,
+		code_actions.gitsigns,
+		hover.dictionary,
+		diagnostics.vale.with({
+			args = function(params)
+				return {
+					--'--config=' .. home .. '/.config/vale/vale.ini',
+					"--no-exit",
+					"--output",
+					"JSON",
+					"--ext",
+					"." .. vim.fn.fnamemodify(params.bufname, ":e"),
+				}
+			end,
+		}),
+	},
+	on_attach = function(_) -- client
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+	end,
 })
